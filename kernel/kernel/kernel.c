641d448913af0391d6db3ee9101bf07b74c4d7ce
Teiroba <114053995+Teiroba@users.noreply.github.com>
2023-03-10 09:45:29 +0100

    Added meaty skeleton

---
+++ kernel/kernel/kernel.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include <kernel/tty.h>
+
+void kernel_main(void) {
+	terminal_initialize();
+	printf("Hello, kernel World!\nThis is a test\n");
+	printf("Testing %s %s %cnd again.\n", "again", "and again", 'a');
+}
