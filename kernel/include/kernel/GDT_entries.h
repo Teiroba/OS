07333f50bf6f1d505da82dd934d3610944390117
Teiroba <114053995+Teiroba@users.noreply.github.com>
2023-03-16 15:04:12 +0100

    fixed missing include

--- kernel/include/kernel/GDT_entries.h
+++ kernel/include/kernel/GDT_entries.h
@@ -2,6 +2,7 @@
 #define _KERNEL_GDT_ENTRIES_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 void GDT_init(void);
 void encodeGDTentry(uint8_t *target, uint32_t base, uint32_t limit, uint16_t flag);
