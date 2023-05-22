0da9b52818cf7bfee9683f4f0d777a9fc58c87bb
Teiroba <114053995+Teiroba@users.noreply.github.com>
2023-05-22 15:28:15 +0200

    aaaaa

--- kernel/arch/i386/isr.c
+++ kernel/arch/i386/isr.c
@@ -8,5 +8,6 @@ __attribute__((noreturn))
 void exception_handler(void);
 void exception_handler() {
     printf("Exception!\n");
-   //__asm__ volatile ("cli; hlt"); // Completely hangs the computer
+   __asm__ volatile ("cli; hlt"); // Completely hangs the computer
+
 }
